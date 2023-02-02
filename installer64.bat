b790fd38311672bde7d501cb41163770854beca1
Deresh <106701009+vDeresh@users.noreply.github.com>
2023-02-02 22:40:11 +0100

    v1.0.1
    Installers added

---
+++ installer64.bat
@@ -0,0 +1,15 @@
+@echo Click any key to start Python download (Python 3.11.1 64-bit)
+@pause>nul
+@start https://www.python.org/ftp/python/3.10.9/python-3.10.9-amd64.exe
+@cls
+@echo Click any key to continue downloading (if Python is installed)
+@pause>nul
+@cls
+@echo Installing required libs
+python.exe -m pip install --upgrade pip
+pip3 install wheel
+pip3 install pygame
+pip3 install PyQt5
+@echo Instalation complete!
+@echo Click any key to close this window
+@pause>nul
