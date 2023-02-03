c858085c68c47719d48d0c569ccc927130d4db4b
Deresh <106701009+vDeresh@users.noreply.github.com>
2023-02-03 11:15:22 +0100

    03.02.2023 | 11:15

---
+++ installer.bat
@@ -0,0 +1,9 @@
+@title Installing required libs
+@echo Installing required libs
+python.exe -m pip install --upgrade pip
+pip3 install wheel
+pip3 install pygame
+pip3 install PyQt5
+@echo Instalation complete!
+@echo Click any key to close this window
+@pause>nul
